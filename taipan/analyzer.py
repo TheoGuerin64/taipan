3aabd45ed47d7f50495caad4ec021abaca3c5d40
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-07-27 22:32:53 +0200

    Add function privacy to analyzer

--- taipan/analyzer.py
+++ taipan/analyzer.py
@@ -21,10 +21,10 @@ from taipan.symbol_table import SymbolTable
 
 
 def analyze(file: Path, ast: AST) -> None:
-    visit(file, deque(), ast.root)
+    _visit(file, deque(), ast.root)
 
 
-def is_defined(symbol_tables: deque[SymbolTable], identifier: Identifier) -> bool:
+def _is_defined(symbol_tables: deque[SymbolTable], identifier: Identifier) -> bool:
     for table in reversed(symbol_tables):
         symbol = table.lookup(identifier.name)
         if not symbol:
@@ -34,35 +34,35 @@ def is_defined(symbol_tables: deque[SymbolTable], identifier: Identifier) -> boo
     return False
 
 
-def visit(file: Path, symbol_tables: deque[SymbolTable], node: Node) -> None:
+def _visit(file: Path, symbol_tables: deque[SymbolTable], node: Node) -> None:
     match node:
         case Program():
-            visit(file, symbol_tables, node.block)
+            _visit(file, symbol_tables, node.block)
         case Block():
             symbol_tables.append(node.symbol_table)
             for statement in node.statements:
-                visit(file, symbol_tables, statement)
+                _visit(file, symbol_tables, statement)
             symbol_tables.pop()
         case If() | While():
-            visit(file, symbol_tables, node.condition)
-            visit(file, symbol_tables, node.block)
+            _visit(file, symbol_tables, node.condition)
+            _visit(file, symbol_tables, node.block)
         case Input():
-            visit(file, symbol_tables, node.identifier)
+            _visit(file, symbol_tables, node.identifier)
         case Print():
-            visit(file, symbol_tables, node.value)
+            _visit(file, symbol_tables, node.value)
         case Assignment():
-            visit(file, symbol_tables, node.identifier)
-            visit(file, symbol_tables, node.expression)
+            _visit(file, symbol_tables, node.identifier)
+            _visit(file, symbol_tables, node.expression)
         case BinaryExpression():
-            visit(file, symbol_tables, node.left)
-            visit(file, symbol_tables, node.right)
+            _visit(file, symbol_tables, node.left)
+            _visit(file, symbol_tables, node.right)
         case UnaryExpression():
-            visit(file, symbol_tables, node.value)
+            _visit(file, symbol_tables, node.value)
         case Comparison():
-            visit(file, symbol_tables, node.left)
-            visit(file, symbol_tables, node.right)
+            _visit(file, symbol_tables, node.left)
+            _visit(file, symbol_tables, node.right)
         case Identifier():
-            if not is_defined(symbol_tables, node):
+            if not _is_defined(symbol_tables, node):
                 raise TaipanSemanticError(
                     file,
                     node.line,
